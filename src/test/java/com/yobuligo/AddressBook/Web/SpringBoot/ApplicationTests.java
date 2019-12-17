bad8ae52509ffcf417692a3d6440fcfb93abd268
Peter <hoffmannpeter82@gmail.com>
2019-12-17 17:31:39 +0100

    Initial Commit

---
+++ src/test/java/com/yobuligo/AddressBook/Web/SpringBoot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yobuligo.AddressBook.Web.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
